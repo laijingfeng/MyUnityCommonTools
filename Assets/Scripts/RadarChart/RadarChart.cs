f912a6aef80f9e9fc8e97d1bf611f38d9c1927e2
laijingfeng <337292616@qq.com>
2016-08-17 18:21:13 +0800

    更新

--- Assets/Scripts/RadarChart/RadarChart.cs
+++ Assets/Scripts/RadarChart/RadarChart.cs
@@ -10,6 +10,10 @@ public class RadarChart : Graphic
     public Color m_LineColor = Color.black;
     public bool m_DrawLine = false;
 
+    public float m_BoundLineWidth = 1f;
+    public Color m_BoundLineColor = Color.black;
+    public bool m_DrawBoundLine = false;
+
     /// <summary>
     /// 百分比
     /// </summary>
@@ -36,16 +40,16 @@ public class RadarChart : Graphic
         SetAllDirty();
     }
 
-    private Vector2 GetPoint(int idx, bool adjust = true)
+    private Vector2 GetPoint(int idx, bool full = true)
     {
         Vector2 ret = Vector2.zero;
         float angle = 360f / m_Cnt * idx + m_AngleOffset;
         ret.x = 0.5f * m_Rect.width * Mathf.Cos(angle * Mathf.Deg2Rad);
         ret.y = 0.5f * m_Rect.height * Mathf.Sin(angle * Mathf.Deg2Rad);
-        if (adjust)
+        ret += m_Rect.center;
+        if (!full)
         {
             ret *= m_Percents[idx];
-            ret += m_Rect.center;
         }
         return ret;
     }
@@ -64,9 +68,9 @@ public class RadarChart : Graphic
 
         for (int i = 0; i < m_Cnt; i++)
         {
-            vh.AddVert(GetPoint(i), color, Vector2.zero);
+            vh.AddVert(GetPoint(i, false), color, Vector2.zero);
         }
-        vh.AddVert(m_Rect.center, color, Vector2.zero);
+        vh.AddVert(Vector2.zero, color, Vector2.zero);
 
         for (int i = 0; i < m_Cnt; i++)
         {
@@ -77,22 +81,30 @@ public class RadarChart : Graphic
         {
             for (int i = 0; i < m_Cnt; i++)
             {
-                vh.AddUIVertexQuad(GetLine(Vector2.zero, GetPoint(i, false)));
+                vh.AddUIVertexQuad(GetLine(Vector2.zero, GetPoint(i, true), m_LineWidth, m_LineColor));
+            }
+        }
+
+        if (m_DrawBoundLine)
+        {
+            for (int i = 0; i < m_Cnt; i++)
+            {
+                vh.AddUIVertexQuad(GetLine(GetPoint(i, false), GetPoint((i + 1) % m_Cnt, false), m_BoundLineWidth, m_BoundLineColor));
             }
         }
     }
 
-    private UIVertex[] GetLine(Vector2 s, Vector2 e)
+    private UIVertex[] GetLine(Vector2 s, Vector2 e, float width, Color color)
     {
         UIVertex[] vers = new UIVertex[4];
         Vector2 v1 = e - s;
         Vector2 v2 = (v1.y == 0f) ? new Vector2(0f, 1f) : new Vector2(1f, -v1.x / v1.y);
         v2.Normalize();
-        v2 *= 0.5f * m_LineWidth;
+        v2 *= 0.5f * width;
         for (int i = 0; i < 4; i++)
         {
             vers[i] = UIVertex.simpleVert;
-            vers[i].color = m_LineColor;
+            vers[i].color = color;
             vers[i].uv0 = Vector2.zero;
             if (i == 0)
             {
