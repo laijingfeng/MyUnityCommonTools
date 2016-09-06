75d31ab8230a4156508b89c1def78479ef45965e
laijingfeng <337292616@qq.com>
2016-09-06 08:05:09 +0800

    snake

---
+++ Assets/Snake/ScrollCircle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScrollCircle : ScrollRect
+{
+    protected float mRadius = 0f;
+
+    protected override void Start()
+    {
+        base.Start();
+        mRadius = (transform as RectTransform).sizeDelta.x * 0.5f;
+    }
+
+    public override void OnDrag(UnityEngine.EventSystems.PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+        var contentPostion = this.content.anchoredPosition;
+        if (contentPostion.magnitude > mRadius)
+        {
+            contentPostion = contentPostion.normalized * mRadius;
+            SetContentAnchoredPosition(contentPostion);
+        }
+    }
+}
---
+++ Assets/Snake/Snake.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Snake : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
