483fe4c5d012ed4cfe7e19e3c1db4ecd4a21bce0
laijingfeng <337292616@qq.com>
2017-01-19 22:06:44 +0800

    update

--- Assets/ReflectionHelper/Test/ReflectionTest.cs
+++ Assets/ReflectionHelper/Test/ReflectionTest.cs
@@ -1,15 +1,14 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class ReflectionTest : MonoBehaviour {
+public class ReflectionTest : MonoBehaviour
+{
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    void Update()
+    {
+
+    }
 }
