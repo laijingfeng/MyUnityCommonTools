4356e17c67c163e73832ae9f668583ef98c0680c
laijingfeng <337292616@qq.com>
2017-04-06 20:02:34 +0800

    搬迁
    注：引起了一些需要整理的地方

--- Assets/GestureJugde/GestureJudge.cs
+++ Assets/GestureJugde/GestureJudge.cs
@@ -87,7 +87,7 @@ public class GestureJudge : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             m_TouchStarted = true;
-            m_TouchStartPos = JerryUtil.GetClickPos();
+            m_TouchStartPos = Vector3.zero;//JerryUtil.GetClickPos();
         }
         else if (Input.GetMouseButtonUp(0))
         {
@@ -149,7 +149,7 @@ public class GestureJudge : MonoBehaviour
     private void Judge()
     {
         Vector3 start = m_TouchStartPos;
-        Vector3 end = JerryUtil.GetClickPos();
+        Vector3 end = Vector3.zero;//JerryUtil.GetClickPos();
 
         float dis = Vector2.Distance(start, end);
 
