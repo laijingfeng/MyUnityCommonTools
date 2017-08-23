8db69af8870ea87c9584d719c192113f9fcaaec8
laijingfeng <337292616@qq.com>
2017-08-23 21:12:40 +0800

    整理

--- Assets/AssetsSettings/Editor/AssetRule.cs
+++ Assets/AssetsSettings/Editor/AssetRule.cs
@@ -1,6 +1,6 @@
-using UnityEngine;
-using UnityEditor;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
 
 namespace Jerry
 {
--- Assets/AssetsSettings/Editor/ImportSetting_Base.cs
+++ Assets/AssetsSettings/Editor/ImportSetting_Base.cs
@@ -1,7 +1,7 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-using UnityEditor;
+﻿using System.Collections.Generic;
 using System.IO;
+using UnityEditor;
+using UnityEngine;
 
 namespace Jerry
 {
